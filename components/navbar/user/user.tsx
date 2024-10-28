f8452aeb0ccc29fb2ca71edc48107f0c93c78dc5
mrcoder57 <tiwariji2300@gmail.com>
2024-10-28 19:01:15 +0530

    feature: add users options

---
+++ components/navbar/user/user.tsx
@@ -0,0 +1,18 @@
+import { nunitoSans } from '@/app/fonts/font'
+import Image from 'next/image'
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className=' flex flex-row items-center justify-center gap-x-3'>
+        <Image src={'/avatar.svg'} alt='logo' width={44} height={44}/>
+        <div className=' flex flex-col items-center justify-center gap-y-1'>
+            <h4 className={`${nunitoSans.className} text-[14px] font-semibold`}>Nitin</h4>
+            <h5 className={`${nunitoSans.className} text-[12px] text-[#565656] font-semibold`}>Nitin</h5>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default User
