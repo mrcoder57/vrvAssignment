240812e6fb330b16dc532014dc474fbf7f29f744
mrcoder57 <tiwariji2300@gmail.com>
2024-10-28 19:47:36 +0530

    feature: create filters section

---
+++ components/ordersPage/filters.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Filters = () => {
+  return (
+    <div className=' w-[818px] h-[70px] flex items-center justify-center bg-white flex-row rounded-lg'>
+
+    </div>
+  )
+}
+
+export default Filters
