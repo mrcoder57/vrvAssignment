9330aebf57abebd57512dcba2aa4915add1c8cf9
mrcoder57 <tiwariji2300@gmail.com>
2024-10-29 21:00:28 +0530

    feature: create stocks comp

--- components/stocksPage/stocks.tsx
+++ components/stocksPage/stocks.tsx
@@ -1,9 +1,23 @@
+import { nunitoSans } from '@/app/fonts/font'
 import React from 'react'
+import StocksTables from './stocksTable'
 
-const stocks = () => {
+const Stocks = () => {
   return (
-    <div>stocks</div>
+    <div className=" flex lg:flex-col flex-wrap w-full  ">
+    <div className=" py-6 flex items-center lg:px-6 px-3 ">
+      <h1
+        className={`${nunitoSans.className} text-[28px] font-bold text-center `}
+      >
+        Product Stock
+      </h1>
+    </div>
+    <div className="flex  items-start justify-around w-full h-full bg-[#F5F6FA] gap-6 lg:px-6 px-3 ">
+      {/* Ensuring the cards are spread out */}
+      <StocksTables/>
+    </div>
+    </div>
   )
 }
 
-export default stocks
+export default Stocks
