12ad2a304c553fd1cf691df2f60ceae52f8466d6
mrcoder57 <tiwariji2300@gmail.com>
2024-10-29 14:39:23 +0530

    fix: adjust size for small devices

--- components/ordersPage/order-by.tsx
+++ components/ordersPage/order-by.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,9 +8,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { nunitoSans } from '@/app/fonts/font';
-import Image from 'next/image';
-import { Button } from '../ui/button';
+import { nunitoSans } from "@/app/fonts/font";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 const Orderby = () => {
   // Track multiple selected buttons in an array
@@ -24,57 +24,70 @@ const Orderby = () => {
     "Quality",
     "Compliance",
     "Risk Management",
-    "Operations"
+    "Operations",
   ];
 
   const toggleButtonSelection = (label: string) => {
-    setSelectedButtons(prevSelected =>
-      prevSelected.includes(label)
-        ? prevSelected.filter(item => item !== label)  // Remove if already selected
-        : [...prevSelected, label]                     // Add if not selected
+    setSelectedButtons(
+      (prevSelected) =>
+        prevSelected.includes(label)
+          ? prevSelected.filter((item) => item !== label) // Remove if already selected
+          : [...prevSelected, label] // Add if not selected
     );
   };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex flex-row items-center gap-x-2 focus:outline-none">
-        <p className={`${nunitoSans.className} text-[14px] font-semibold`}> Orders By</p>
-        <Image src={'/dropdown.svg'} width={20} height={24} alt='arrow-down' />
+        <p className={`${nunitoSans.className} text-[14px] font-semibold`}>
+          {" "}
+          Orders By
+        </p>
+        <Image src={"/dropdown.svg"} width={20} height={24} alt="arrow-down" />
       </DropdownMenuTrigger>
-      
-      <DropdownMenuContent className="w-[420px] h-[250px] py-1 bg-white rounded-lg shadow-md flex flex-col gap-y-1">
-        <DropdownMenuLabel className={`${nunitoSans.className} text-[14px] font-semibold`}>
+
+      <DropdownMenuContent className="lg:w-[420px] w-[320px] h-[250px] py-1 bg-white rounded-lg shadow-md flex flex-col gap-y-1">
+        <DropdownMenuLabel
+          className={`${nunitoSans.className} text-[14px] font-semibold`}
+        >
           Select Order Type
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        
+
         <div className="flex flex-wrap items-center justify-center gap-x-[13px] gap-y-[13px] pt-3 pb-1">
           {buttons.map((label, index) => (
             <Button
               key={index}
               onClick={() => toggleButtonSelection(label)}
               className={`w-[120px] h-[30px] rounded-[17px] shadow-none border hover:bg-white border-[#979797] ${
-                selectedButtons.includes(label) ? 'bg-[#4880FF] text-white' : 'bg-white text-black'
+                selectedButtons.includes(label)
+                  ? "bg-[#4880FF] text-white"
+                  : "bg-white text-black"
               }`}
             >
-              <p className={`${nunitoSans.className} text-[12px] font-medium`}>{label}</p>
+              <p className={`${nunitoSans.className} text-[12px] font-medium`}>
+                {label}
+              </p>
             </Button>
           ))}
         </div>
-        
+
         <DropdownMenuSeparator />
-        
+
         <div className="flex items-center justify-center">
           <Button
             variant="outline"
             className="w-[120px] h-[34px] shadow-none bg-[#4880FF] text-white border border-[#979797]"
           >
-            <p className={`${nunitoSans.className} text-[12px] font-semibold`}> Apply Now </p>
+            <p className={`${nunitoSans.className} text-[12px] font-semibold`}>
+              {" "}
+              Apply Now{" "}
+            </p>
           </Button>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
 
 export default Orderby;
