e38264cbab1e043cb155d48859792ce6a2f6bc13
mrcoder57 <tiwariji2300@gmail.com>
2024-11-01 13:58:59 +0530

    fix: icon sizes

--- components/navbar/user/user.tsx
+++ components/navbar/user/user.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const User = () => {
   return (
     <div className=' flex flex-row items-center justify-center gap-x-3'>
-        <Image src={'/avatar.svg'} alt='logo' width={44} height={44}/>
-        <div className=' lg:flex hidden flex-col items-center justify-center gap-y-1'>
+        <Image src={'/avatar.svg'} alt='logo' width={35} height={35}/>  
+        <div className=' lg:flex hidden flex-col items-center justify-center '>
             <h4 className={`${nunitoSans.className} text-[14px] font-semibold`}>Nitin</h4>
             <h5 className={`${nunitoSans.className} text-[12px] text-[#565656] font-semibold`}>Admin</h5>
         </div>
