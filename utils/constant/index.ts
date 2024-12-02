cb58a24b2afb17fb32488508bea2da0ea48bc047
mrcoder57 <tiwariji2300@gmail.com>
2024-12-02 07:32:13 +0530

    feature: update mockUsers

--- utils/constant/index.ts
+++ utils/constant/index.ts
@@ -20,3 +20,20 @@ export const TableData = [
     { id: 'INV003', name: 'Tom Brown', address: '789 Oak St', date: '2023-10-10', amount: '$350.00', status: 'Overdue' },
     // Add more sample rows as needed
   ];
+ export const mockUserData = [
+      { id: '1', name: 'John Doe', email: 'johndoe@example.com', role: 'Admin', permissions: ['View', 'Edit', 'Delete'] },
+      { id: '2', name: 'Jane Smith', email: 'janesmith@example.com', role: 'Analyst', permissions: ['View', 'Edit'] },
+      { id: '3', name: 'Tom Brown', email: 'tombrown@example.com', role: 'Inventory', permissions: ['View', 'Delete'] },
+      { id: '4', name: 'Emily Green', email: 'emilygreen@example.com', role: 'Admin', permissions: ['View', 'Edit', 'Delete'] },
+      { id: '5', name: 'Michael Johnson', email: 'michaeljohnson@example.com', role: 'Analyst', permissions: ['View', 'Edit'] },
+      { id: '6', name: 'Sarah Lee', email: 'sarahlee@example.com', role: 'User', permissions: ['View', 'Delete'] },
+      { id: '7', name: 'David Williams', email: 'davidwilliams@example.com', role: 'Inventory', permissions: ['View', 'Edit'] },
+      { id: '8', name: 'Olivia Brown', email: 'oliviabrown@example.com', role: 'Admin', permissions: ['View', 'Edit', 'Delete'] },
+      { id: '9', name: 'Jennifer White', email: 'jenniferwhite@example.com', role: 'Inventory', permissions: ['View', 'Edit'] },
+      { id: '10', name: 'Michael Johnson', email: 'michaeljohnson@example.com', role: 'Analyst', permissions: ['View', 'Edit'] },
+      { id: '11', name: 'Sarah Lee', email: 'sarahlee@example.com', role: 'User', permissions: ['View', 'Delete'] },
+      { id: '12', name: 'David Williams', email: 'davidwilliams@example.com', role: 'Admin', permissions: ['View', 'Edit'] },
+      { id: '13', name: 'Olivia Brown', email: 'oliviabrown@example.com', role: 'Admin', permissions: ['View', 'Edit', 'Delete'] },
+      { id: '14', name: 'Jennifer White', email: 'jenniferwhite@example.com', role: 'Analyst', permissions: ['View', 'Edit'] },
+    ];
+    
