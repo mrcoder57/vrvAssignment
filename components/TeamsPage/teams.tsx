6eef0a4561a5e58e322fa5d87d5a3c14d2bd1edb
mrcoder57 <tiwariji2300@gmail.com>
2024-12-04 07:40:08 +0530

    change color

--- components/TeamsPage/teams.tsx
+++ components/TeamsPage/teams.tsx
@@ -1,23 +1,23 @@
-import { nunitoSans } from '@/app/fonts/font'
-import React from 'react'
-import AddMember from './addMember'
+import { nunitoSans } from "@/app/fonts/font";
+import React from "react";
+import AddMember from "./addMember";
 
 const Teams = () => {
   return (
     <div className=" flex lg:flex-col flex-wrap w-full  ">
-    <div className=" py-6 flex items-center lg:px-6 px-3 ">
-      <h1
-        className={`${nunitoSans.className} text-[28px] font-bold text-center `}
-      >
-            Add Team Member
-      </h1>
+      <div className=" py-6 flex items-center lg:px-6 px-3 ">
+        <h1
+          className={`${nunitoSans.className} text-[28px] font-bold text-center `}
+        >
+          Add Team Member
+        </h1>
+      </div>
+      <div className="flex  items-start justify-around w-full h-full bg-[#F5F6FA] gap-6 lg:px-6 px-3 ">
+        {/* Ensuring the cards are spread out */}
+        <AddMember />
+      </div>
     </div>
-    <div className="flex  items-start justify-around w-full h-full bg-[#F5F6FA] gap-6 lg:px-6 px-3 ">
-      {/* Ensuring the cards are spread out */}
-   <AddMember/>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Teams
+export default Teams;
--- components/yourTeamsPage/badges/badge.tsx
+++ components/yourTeamsPage/badges/badge.tsx
@@ -5,7 +5,7 @@ interface BadgeProps {
 }
 const Badge = ({ content }: BadgeProps) => {
   return (
-    <div className=' flex items-center justify-center h-[30px] bg-lime-100 w-auto px-1 rounded-sm shadow-sm'>
+    <div className=' flex items-center justify-center h-[30px] bg-blue-200 w-auto px-1 rounded-sm shadow-sm'>
         <p className={`${nunitoSans.className} text-xs text-black`}>{content}</p>
     </div>
   )
